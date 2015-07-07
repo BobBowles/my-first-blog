78ebe0d48c986eb6defe30f9fde07ace6d8534d7
Bob Bowles <bobjohnbowles@gmail.com>
2015-07-07 16:40:45 +0100

    Add comment feature - DjangoGirls Extension

--- blog/models.py
+++ blog/models.py
@@ -18,5 +18,33 @@ class Post(models.Model):
         self.date_published = timezone.now()
         self.save()
         
+    def approved_comments(self):
+        """
+        List the comments on this post that have been approved.
+        """
+        return self.comments.filter(approved=True)
+        
     def __str__(self):
         return self.title
+        
+        
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    
+    # option here for registered users only making comments 
+    # - other changes needed for this to work, eg, registration page for new 
+    # users
+    #author = models.ForeignKey('auth.User')
+    author = models.CharField(max_length=200)
+    
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved = models.BooleanField(default=False)
+    
+    def approve(self):
+        self.approved = True
+        self.save()
+        
+    def __str__(self):
+        return self.text
+    
